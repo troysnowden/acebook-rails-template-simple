1cf83b109836c52f987864cb48dc7262d8f88fd7
Tom Baxter <thomas99baxter@gmail.com>
2021-11-04 14:03:56 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   
   root 'home#index'
-  get 'home/test-bar'
-=======
-  root 'landing#index'
->>>>>>> main
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
