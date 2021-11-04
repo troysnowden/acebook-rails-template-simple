db94a4ae9826c86db22728c0354ff3c082e59b3c
amin-newcastle <muhammad-karim@hotmail.com>
2021-11-04 10:58:33 +0000

    added user with scaffold

---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "users/show", type: :view do
+  before(:each) do
+    @user = assign(:user, User.create!(
+      fullname: "Fullname",
+      username: "Username",
+      password: "Password"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Fullname/)
+    expect(rendered).to match(/Username/)
+    expect(rendered).to match(/Password/)
+  end
+end
