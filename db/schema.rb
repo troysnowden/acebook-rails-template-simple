22804232601b022c3cb32e0fe0203db363ab1390
Troy Snowden <troybradsnowden@gmail.com>
2021-11-05 11:56:15 +0000

    Change FK from singular to plural

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_114734) do
+ActiveRecord::Schema.define(version: 2021_11_05_115506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string "comment"
-    t.bigint "posts_id", null: false
-    t.bigint "users_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["posts_id"], name: "index_comments_on_posts_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-    t.index ["users_id"], name: "index_comments_on_users_id"
   end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,8 +40,5 @@ ActiveRecord::Schema.define(version: 2021_11_05_114734) do
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "posts", column: "posts_id"
   add_foreign_key "comments", "users"
-  add_foreign_key "comments", "users", column: "users_id"
-  add_foreign_key "posts", "users"
 end
